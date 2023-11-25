84dc5e62ecddf1968b684e8fe41a6d9c2820cc4f
SmrutiBachhav <smrutib2006@gmail.com>
2023-11-25 19:17:01 +0530

    que

--- tutorial/problem2.java
+++ tutorial/problem2.java
@@ -1,16 +1,23 @@
 package folder.folderl1.folderl2;
-
 class  Calculator{
-    
     Calculator(int a,int b){
-        
-        System.out.println(a+b);
+        System.out.println("Sum of a and  b:"+(a+b));
+    }
+}
+class Sc_calculator{
+    Sc_calculator(int a,int b){
+        System.out.println("Sum of sina and sinb :"+(Math.sin(a)+Math.sin(b)));
+    }
+}
+class Hyb_calculator{
+    Hyb_calculator(int a,int b){
+        System.out.println("Sum of sina and b is :"+(Math.sin(a)+b));
     }
 }
 public class problem2{
-    protected int proInt=9;
-    int defInt=10;
     public static void main (String[]args){
         Calculator c=new Calculator(4,6);
+        Sc_calculator scc=new Sc_calculator(90,90);
+        Hyb_calculator hybc =new Hyb_calculator(90,2);
     }
 }
