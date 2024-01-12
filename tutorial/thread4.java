0761e310b310732782522cfa298e7c46934491b1
SmrutiBachhav <smrutib2006@gmail.com>
2024-01-12 12:14:53 +0530

    Creating thread using thread constructor Thread(Runnable r, String name)

---
+++ tutorial/thread4.java
@@ -0,0 +1,16 @@
+//thread constructors --Thread(Runnable r, String name)
+class MyThread implements Runnable{
+    
+    public void run(){       
+        System.out.println("Setting Thread...");     
+    }
+}
+class thread4{
+    public static void main(String[]args){
+        Runnable r=new MyThread();
+        Thread t=new Thread(r,"Sam");
+        t.start();
+        System.out.println("the name of the thread:" + t.getName());
+        System.out.println("the id of the thread:" + t.getId());
+}
+}
